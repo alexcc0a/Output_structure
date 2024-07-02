18c7414300aa2b44842aee6a58c97db72a211474
Alexander Nesterov <alex18nest@icloud.com>
2024-07-02 17:27:00 -0300

    Initial Commit

---
+++ Output_structure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Output_structure
+//
+//  Created by Александр Нестеров on 02/07/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
